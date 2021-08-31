92cb743abd62de2593dcf61052d89bd1519c2287
vuquangmt <vuquang11gbg@gmail.com>
2021-08-31 10:26:42 +0700

    baitap2

---
+++ baitap2/Controllers/Demobaitap2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitap2.Controllers
+{
+    public class Demobaitap2Controller : Controller
+    {
+        // GET: Demobaitap2
+        public ActionResult GPT()
+        {
+            return View();
+        }
+        [HttpPost]
+    }
+}
--- baitap2/Models/GPT.cs
+++ baitap2/Models/GPT.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Mvc;
 
-namespace baitap2.Models
+namespace baitap2.controllers
+
 {
     public class GPT
     {
+        //giai phuong trinh bac 1
+        public
     }
 }
