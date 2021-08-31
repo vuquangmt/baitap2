1badd2c0cb524aa29515b711f388b04be6b432cb
vuquangmt <vuquang11gbg@gmail.com>
2021-08-31 10:47:21 +0700

    ABC

--- baitap2/Controllers/Demobaitap2Controller.cs
+++ baitap2/Controllers/Demobaitap2Controller.cs
@@ -9,10 +9,25 @@ namespace baitap2.Controllers
     public class Demobaitap2Controller : Controller
     {
         // GET: Demobaitap2
-        public ActionResult GPT()
+        public ActionResult GPT1(String soa, String sob)
         {
+            Double a, b;
+            String ketqua = " ";
+            a = Convert.ToDouble(soa);
+            b = Convert.ToDouble(sob);
+
+            if (a == 0)
+            {
+                ketqua = ("Phuong Trinh Vo Nghiem");
+            }
+            else
+            {
+                Double x = -b / a;
+                ketqua = ("Vay nghiem cua phuong trinh la:  " + x);
+            }
+
+            ViewBag.abc = ketqua;
             return View();
         }
-        [HttpPost]
     }
 }
